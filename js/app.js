d939882d856330803261c2182222ded4b70abecf
m-jozefowicz <mjozefowicz.wso@gmail.com>
2017-04-21 14:42:53 +0200

    Added slider with chairs to section2 ~MJ

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 $(function() {
 
+  // simply drop down menu in nav section
   function dropDownMenu() {
     var firstLiMenu = $(".main-menu").find("li").first();
 
@@ -12,6 +13,8 @@ $(function() {
   }
    dropDownMenu();
 
+
+   // fadeIn/Out effect on title box in section 3
   function animateMiniBoxImg() {
     var chairBox = $(".chair-box")
     var miniBox = $(".chair-box-shdw");
@@ -25,5 +28,48 @@ $(function() {
 
   }
    animateMiniBoxImg()
+
+});
+
+document.addEventListener("DOMContentLoaded", function(){
+
+  // SLIDER in pure javascript
+
+  //
+  var buttonNext = document.getElementById("arrow-right");
+  var buttonPrev = document.getElementById("arrow-left");
+  console.log(buttonNext);
+  console.log(buttonPrev);
+
+  var allLiElem = document.querySelectorAll("#images-slider li");
+  console.log(allLiElem);
+
+  var indexImg = 0;
+  //
+
+  allLiElem[indexImg].classList.add("visible");
+  // console.log(indexImg);
+
+  //
+
+  buttonNext.addEventListener("click", function() {
+    allLiElem[indexImg].classList.remove("visible");
+    if(indexImg == allLiElem.length-1) {
+      indexImg = -1;
+    }
+    indexImg++;
+    allLiElem[indexImg].classList.add("visible");
+  });
+  //
+
+  buttonPrev.addEventListener("click", function() {
+    allLiElem[indexImg].classList.remove("visible");
+    if (indexImg < 0) {
+     indexImg=0;
+     indexImg++;
+    }
+    indexImg--;
+    allLiElem[indexImg].classList.add("visible");
+  });
 
 });
